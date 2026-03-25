fc3a7504d42bca6464f7a81ec77e1860c153259d
velagaprasanth0826 <velangaprasanth@loopinsocial.in>
2026-03-25 17:12:04 +0530

    Remove unused navigate variable from Hero

--- src/pages/Home/components/Hero/Hero.jsx
+++ src/pages/Home/components/Hero/Hero.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import "./hero.scss";
-import { useNavigate } from 'react-router-dom';
 import image1 from '../../../../assets/images/image1.png';
 import image2 from '../../../../assets/images/image2.png';
 import image3 from '../../../../assets/images/image3.png';
@@ -8,7 +7,6 @@ import image4 from '../../../../assets/images/image4.png';
 import image5 from '../../../../assets/images/image5.png';
 
 const Hero = () => {
-    const navigate = useNavigate()
     const [currentSlide, setCurrentSlide] = useState(0);
     const slides = [image1, image2, image3, image4, image5];
 
